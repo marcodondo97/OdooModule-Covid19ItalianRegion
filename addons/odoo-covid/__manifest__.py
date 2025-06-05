25f643edc09f40bef72e360e1b9fba086b38bf08
marcodondo97 <marco.dondo97@gmail.com>
2025-06-05 21:22:19 +0200

    WIP - control data limti and manifest file

--- addons/odoo-covid/__manifest__.py
+++ addons/odoo-covid/__manifest__.py
@@ -1,16 +1,27 @@
-# __manifest__.py
 {
-    'name': "covid-italy-regions",
-    'summary': "Import and show SARS-CoV-2 data per Italian region",
-    'version': '1.0',
+    'name': "COVID-19 Italy Regions Data",
+    'summary': "Import and display updated SARS-CoV-2 data by Italian region",
+    'version': '1.0.1',
     'author': 'Marco Dondo',
+    'website': 'https://github.com/marcodondo97/odoo-covid',
     'category': 'Health',
+    'license': 'LGPL-3',
     'installable': True,
     'application': True,
-    'license': 'LGPL-3',
+    'auto_install': False,
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
         'views/covid_data_views.xml',
     ],
+    'images': [
+        'static/description/icon.png',
+    ],
+    'external_dependencies': {
+        'python': ['requests'],
+    },
+    'description': """
+Odoo module to import and display up-to-date COVID-19 data for Italian regions.
+Includes dynamic date search and aggregated data views.
+    """,
 }
--- addons/odoo-covid/models/date_filter_wizard.py
+++ addons/odoo-covid/models/date_filter_wizard.py
@@ -1,11 +1,12 @@
 from odoo import models, fields, exceptions, _
 import requests
 import logging
-from datetime import timedelta
+from datetime import timedelta, date
 
 _logger = logging.getLogger(__name__)
 
 GITHUB_PROVINCE_URL = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province.json"
+MIN_DATE = date(2020, 2, 24)  # February 24th, 2020
 
 class CovidDateFilterWizard(models.TransientModel):
     _name = 'covid.date.filter.wizard'
@@ -15,7 +16,7 @@ class CovidDateFilterWizard(models.TransientModel):
     date_end = fields.Date(string='End Date', required=True)
 
     def _fetch_and_store_data_from_json(self, date_str, json_data):
-        #Extracts data for the specified date and saves totals by region
+        # Extracts data for the specified date and saves totals by region
         covid_data_model = self.env['covid.region.data']
         region_totals = {}
 
@@ -49,6 +50,7 @@ class CovidDateFilterWizard(models.TransientModel):
             'res_model': 'covid.region.data',
             'view_mode': 'list,form',
             'domain': domain,
+            'create': False,
             'context': {
                 'search_disable_custom_filters': True,
                 'search_bar_visible': False,
@@ -57,13 +59,26 @@ class CovidDateFilterWizard(models.TransientModel):
         }
 
     def apply_filter(self):
-        #Applies the date filter and loads missing data if needed
-        domain = []
-        if self.date_start:
-            domain.append(('date', '>=', self.date_start))
-        if self.date_end:
-            domain.append(('date', '<=', self.date_end))
+        # Applies the date filter and loads missing data if needed
+        # Validations
+        if self.date_start and self.date_start < MIN_DATE:
+            raise exceptions.UserError(_(
+                "Data is available only from February 24th, 2020. "
+                "Please select a start date on or after this date."
+            ))
 
+        if self.date_end and self.date_end < MIN_DATE:
+            raise exceptions.UserError(_(
+                "End date cannot be earlier than February 24th, 2020."
+            ))
+
+        if self.date_end < self.date_start:
+            raise exceptions.UserError(_("End date cannot be earlier than start date."))
+
+        # Prepare domain
+        domain = [('date', '>=', self.date_start), ('date', '<=', self.date_end)]
+
+        # Download and parse JSON data
         try:
             res = requests.get(GITHUB_PROVINCE_URL)
             res.raise_for_status()
@@ -84,11 +99,19 @@ class CovidDateFilterWizard(models.TransientModel):
                     data_loaded = True
             current_date += timedelta(days=1)
 
+        # Notifications
         if data_loaded:
             self.env['bus.bus']._sendone(self.env.user.partner_id, 'simple_notification', {
                 'title': _('COVID Data'),
                 'message': _('New data loaded successfully!'),
                 'sticky': False,
+            })
+        elif not covid_data_model.search_count(domain):
+            self.env['bus.bus']._sendone(self.env.user.partner_id, 'simple_notification', {
+                'title': _('COVID Data'),
+                'message': _('No data found for the selected date range.'),
+                'sticky': False,
+                'type': 'warning',
             })
 
         return self._build_action(domain)
